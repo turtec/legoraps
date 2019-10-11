2fe402d8b40396cfc8d0fb522d7ec68e13c4af88
Sascha Turowski <turowski@turtec.de>
2019-10-11 10:55:21 +0200

    added motor stop to avoid loosing abttery

--- nodeserver/server.js
+++ nodeserver/server.js
@@ -10,24 +10,22 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'legorasp-control/', 'index.html'));
 });
 
-app.get('/cam2', function(req, res) {
-console.log('ggg');
-  //res.sendFile(path.join(__dirname, 'legorasp-control/', 'index.html'));
-  //res.sendFile(path.join(__dirname, 'legorasp-control/', 'index.html'));
-res.write('data');
-        res.end('end');
+app.get('/cam/right', function(req, res) {
+ const { spawn } = require('child_process');	
+ const pyProg = spawn('python', ['../pycontroler/stepmotor_right.py']);
+ res.end('end');
 });
 
 app.get('/cam/left', function(req, res) {
-  		
-    const { spawn } = require('child_process');	
-    const pyProg = spawn('python', ['../pycontroler/stepmotor.py']);
-    res.end('end');
-    pyProg.stdout.on('data', function(pydata) {
+  console.log('left');
+  const { spawn } = require('child_process');	
+  const pyProg = spawn('python', ['../pycontroler/stepmotor_left.py']);
+  res.end('end');
+  /* pyProg.stdout.on('data', function(pydata) {
 	const output = pydata;	
         console.log(pydata.toString());
         res.write('data' + output); 
-   });		
+   });	*/	
 
 
 
