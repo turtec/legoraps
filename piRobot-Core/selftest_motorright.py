1711fb584e5f499047bf0403b241c745ec8b3bc6
Sascha Turowski <turowski@turtec.de>
2020-09-04 11:18:31 +0200

    added selftest motorright

---
+++ piRobot-Core/selftest_motorright.py
@@ -0,0 +1,43 @@
+from lib.DcMotor import DcMotor
+import time
+import sys
+try:
+    import RPi.GPIO as GPIO
+except ModuleNotFoundError:
+    # Error handling
+    import FakeRPi.GPIO as GPIO
+
+EnableA = 26
+Input1 = 19
+Input2 = 13
+    
+#EnableA = 26
+#Input1 = 19
+#Input2 = 13
+    
+def control(command):
+  if command == "f":
+    motor.forward()
+  if command == "b":
+    motor.backward()
+  if command == "s":
+    motor.stop()
+
+motor = DcMotor('right',EnableA, Input1, Input2,GPIO)
+
+
+
+
+try:
+  while True:
+    val = input('Choose a direction (f)orward,(b)ackward,(s)top,(i)ncrease Speed,(d)ecrease Speed: ')
+    control(val)
+
+except KeyboardInterrupt:
+  GPIO.cleanup()
+  #stop()
+  print('stopped')
+  #
+
+
+
