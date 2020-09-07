ba26ceb4023a9a966e83bc929c23cf8eacfce7c2
Sascha Turowski <turowski@turtec.de>
2020-09-07 16:02:06 +0200

    fixes bugs

--- piRobot-Core/lib/DcMotor.py
+++ piRobot-Core/lib/DcMotor.py
@@ -7,12 +7,14 @@ class DcMotor:
         self.inputPin1 = inputPin1
         self.inputPin2 = inputPin2
         self.GPIO = GPIO
+        self.currentSpeed = 50
         self.GPIO.setmode(self.GPIO.BCM)
-        self.pwm = GPIO.PWM(enablePin,100)
+        self.pwm = GPIO.PWM(enablePin,1000)
         self.GPIO.setup(enablePin, self.GPIO.OUT)
         self.GPIO.setup(inputPin1, self.GPIO.OUT)
         self.GPIO.setup(inputPin2, self.GPIO.OUT)
-        self.pwm.changeDutyCircle(40)
+        self.pwm.start(0)
+        self.pwm.ChangeDutyCycle(self.currentSpeed)
 
     def forward(self):
        print("{} is running forward".format(self.name))
@@ -37,7 +39,7 @@ class DcMotor:
    
        self.currentSpeed = self.currentSpeed + 10
        if self.currentSpeed <= 100:
-         self.pmw.ChangeDutyCycle(self.currentSpeed)
+         self.pwm.ChangeDutyCycle(self.currentSpeed)
        else:
          print('max speed 100')
 
@@ -45,6 +47,6 @@ class DcMotor:
        print ("{} speed decreased".format(self.name))
        self.currentSpeed = self.currentSpeed - 10
        if self.currentSpeed >= 0:
-         self.pmw.ChangeDutyCycle(self.currentSpeed)
+         self.pwm.ChangeDutyCycle(self.currentSpeed)
        else:
          print('min')
